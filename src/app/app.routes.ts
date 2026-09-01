f49ac88e1b1cb09b64faf3dd7ee15db9874f4cd1
Arthur Brasil <brasil2403@gmail.com>
2026-09-01 14:27:44 -0300

    Implement lazy loading for salas routes
    Added lazy loading routes for ListaSalas and DetalhesSala components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Layout } from './shared/components/layout/layout';
 import { Login } from './features/auth/login/login';
 import { Dashboard } from './features/dashboard/dashboard/dashboard';
 import { GradeSemanal } from './features/grade-semanal/grade-semanal';
-
 export const routes: Routes = [
   { path: 'login', component: Login },
   {
@@ -15,6 +14,16 @@ export const routes: Routes = [
       { path: 'dashboard', component: Dashboard },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'grade-semanal', component: GradeSemanal },
+      {
+        path: 'salas',
+        loadComponent: () =>
+          import('./features/salas/lista-salas/lista-salas').then((m) => m.ListaSalas),
+      },
+      {
+        path: 'salas/:id',
+        loadComponent: () =>
+          import('./features/salas/detalhes-sala/detalhes-sala').then((m) => m.DetalhesSala),
+      },
     ],
   },
   { path: '**', redirectTo: 'login' },
