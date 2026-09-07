e5e59bf12e5360db684bf039a899e9c3d4a9d2d2
Vitor Oliveira <vitonciogames@gmail.com>
2026-09-07 01:32:14 -0300

    ♻️  refactor: Atualizar arquivos spec.ts com o provider providerRouter

--- src/app/shared/components/layout/layout.spec.ts
+++ src/app/shared/components/layout/layout.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { Layout } from './layout';
 
 describe('Layout', () => {
@@ -8,7 +8,8 @@ describe('Layout', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Layout]
+      imports: [Layout],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
